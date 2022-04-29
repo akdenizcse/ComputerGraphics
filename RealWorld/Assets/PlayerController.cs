57d8f351bb68825ff08155220685984129400b51
Ozan Engin <ozan.121497@gmail.com>
2022-04-29 18:47:21 +0300

    Big change on character and objects.
    -CharacterController has been changed to Rigidbody (It is much easier to interact with other physical objects with Rigidbody)
    -Rididbody character moves completed
    -character jump completed
    -character run complete
    -completed character hitting and pushing other objects
    -completed character holding and dropping objects (press E to hold and release)

--- RealWorld/Assets/PlayerController.cs
+++ RealWorld/Assets/PlayerController.cs
@@ -2,60 +2,124 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
-    public float MovementSpeed =1;
-    public float Gravity = 9.8f;
-    private float velocity = 0;
-	
-	public float jumpSpeed = 2.0f;
-	private Vector3 movingDirection = Vector3.zero;
-	
+    private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
 
-    private Camera cam;
+    private float xRot;
+
+    [SerializeField] private LayerMask FloorMask;
+    [SerializeField] private Transform FeetTransform;
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private Rigidbody PlayerBody;
+    [Space]
+    [SerializeField] private float Speed;
+    [SerializeField] private float Rush;
+    [SerializeField] private float Sensitivty;
+    [SerializeField] private float Jumpforce;
+
+    public float pickUpRange = 5;
+    public float moveForce = 250;
+    public Transform holdParent;
+    private GameObject heldObj;
+
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        cam = Camera.main;
+        
     }
 
     void Update()
     {
-        // player movement - forward, backward, left, right
-        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
-        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
-        characterController.Move((cam.transform.right * horizontal + cam.transform.forward * vertical) * Time.deltaTime);
 
-        // Gravity
-        if(characterController.isGrounded)
+        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MovePlayerCamera();
+        
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            velocity = 0;
+            if (Physics.CheckSphere(FeetTransform.position, 0.1f, FloorMask))
+            {
+                Speed = Rush;
+            }
         }
         else
         {
-            velocity -= Gravity * Time.deltaTime;
-            characterController.Move(new Vector3(0, velocity, 0));
+            Speed = 5; //burayi düzeltecegim.
         }
-		// Running
-		if (Input.GetKey(KeyCode.LeftShift)) {
- 
-            MovementSpeed = 4;
-            print ("Running");
- 
-        } else {
- 
-            MovementSpeed = 1;
-            print ("Not Running");
- 
+
+        //holding
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj == null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+                    PickupObject(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                DropObject();
+            }
         }
-		// Jumping
-		
-		
-		if (characterController.isGrounded && Input.GetButton("Jump")) {
-			movingDirection.y = jumpSpeed;
-		}
-		movingDirection.y -= Gravity * Time.deltaTime;
-		characterController.Move(movingDirection * Time.deltaTime);
-		
+        if (heldObj != null)
+        {
+            MoveObject();
+        }
+    }
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+        PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Physics.CheckSphere(FeetTransform.position,0.1f, FloorMask)) //check player if on the floor
+            {
+                PlayerBody.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
+            }
+        }
+    }
+    private void MovePlayerCamera()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivty;
+        //Mouse movements
+        transform.Rotate(0f, PlayerMouseInput.x * Sensitivty, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+
+    //pick up objects //
+    void MoveObject()
+    {
+        if (Vector3.Distance(heldObj.transform.position, holdParent.position) > 0.1f)
+        {
+            Vector3 moveDirection = (holdParent.position - heldObj.transform.position);
+            heldObj.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
+        }
+    }
+
+    void PickupObject(GameObject pickObj)
+    {
+        if (pickObj.GetComponent<Rigidbody>())
+        {
+            Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
+            objRig.useGravity = false;
+            objRig.drag = 10;
+
+            objRig.transform.parent = holdParent;
+            heldObj = pickObj;
+        }
+    }
+    void DropObject()
+    {
+        Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
+        heldRig.useGravity = true;
+        heldRig.drag = 1;
+
+        heldObj.transform.parent = null;
+        heldObj = null;
     }
 }
